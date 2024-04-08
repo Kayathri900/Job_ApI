a8af58af9207a2326cacba18453f8e8436b7787a
Kayathri900 <163170719+Kayathri900@users.noreply.github.com>
2024-04-08 23:57:57 +0100

    first commit

--- app.js
+++ app.js
@@ -24,20 +24,24 @@ const jobsRouter=require('./routes/jobs')
 const notFound=require('./middleware/not-found')
 const errorHandlerMiddleware=require('./middleware/error-handler')
 
-app.use(express.json())
 //extre pacakges
 app.set('trust proxy',1)
 app.use(rateLimiter({
 	windowMs: 15 * 60 * 1000, // 15 minutes
 	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 }))
-app.use(xss())
+app.use(express.json())
+
 app.use(helmet())
 app.use(cors())
+app.use(xss())
+
+app.get('/', (req, res) => {
+	res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+  });
 //routes
 app.use('/api/v1/auth',authRouter)
 app.use('/api/v1/jobs',authenticatedUser,jobsRouter)
-app.get('/', (req, res) => {   res.send('Jobs API') })
 
 app.use(notFound)
 app.use(errorHandlerMiddleware)
